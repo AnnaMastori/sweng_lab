91ef2ddf874fec6a18ba3e1bab0e5009cb076746
Anna Mastori <t8190105@aueb.gr>
2022-05-23 21:02:16 +0300

    Add tests for multiply method

--- unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -2,7 +2,9 @@ package math;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class ArithmeticOperationsTest {
 	static ArithmeticOperations testClass;
@@ -30,5 +32,33 @@ public class ArithmeticOperationsTest {
 	public void test_divide_by_zero() {
 		testClass.divide(20.0, 0);
 	}
+	
+	@Test
+	public void test_multiply() {
+		Assert.assertEquals(20, testClass.multiply(5, 4), DELTA);
+	}
+	
+	@Test
+	public void test_multiply_with_zero() {
+		Assert.assertEquals(0.0, testClass.multiply(0, 4), DELTA);
+	}
+	
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
+
+	@Test
+	public void shouldThrowIllegalArgumentExceptionNotInt() {
+	    expectedEx.expect(IllegalArgumentException.class);
+	    expectedEx.expectMessage("The product does not fit in an Integer variable");
+	    testClass.multiply(Integer.MAX_VALUE, 2);
+	}
+	
+	@Test
+	public void shouldThrowIllegalArgumentExceptionNegatives() {
+	    expectedEx.expect(IllegalArgumentException.class);
+	    expectedEx.expectMessage("x & y should be >= 0");
+	    testClass.multiply(-1, 2);
+	}
+	
 
 }
