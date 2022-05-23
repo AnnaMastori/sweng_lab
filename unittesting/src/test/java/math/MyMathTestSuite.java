a376721dc6727f3aa47cc65b6b6aeffc7301a970
Anna Mastori <t8190105@aueb.gr>
2022-05-23 23:05:02 +0300

    Add class MyMathTestSuite

---
+++ unittesting/src/test/java/math/MyMathTestSuite.java
@@ -0,0 +1,9 @@
+package math;
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MyMathTest.class, MyMathParameterizedTest.class})
+public class MyMathTestSuite {
+
+}
