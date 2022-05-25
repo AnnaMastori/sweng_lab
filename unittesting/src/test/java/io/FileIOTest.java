06b23b98cfd172b6abeb07fe0d4905c746a0f1cb
Anna Mastori <t8190105@aueb.gr>
2022-05-25 21:30:53 +0300

    Create tests for IllegalArgumentException of readFile method

--- unittesting/src/test/java/io/FileIOTest.java
+++ unittesting/src/test/java/io/FileIOTest.java
@@ -2,14 +2,16 @@ package io;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import math.ArithmeticOperations;
 
 public class FileIOTest {
 
 	static FileIO testClass;
-	static String fileName;
+	static int nums[] = {1,2,3,4,5};
 
 	@BeforeClass
 	public static void setup() {
@@ -19,8 +21,23 @@ public class FileIOTest {
 	@Test
 	public void test_readFile() {
 		int[] nums= {1,2,3,4,5};
-		
-		
 		Assert.assertArrayEquals(nums, testClass.readFile("../unittesting/src/test/resources/io/testnums.txt"));
+	}
+	
+	@Rule
+	public ExpectedException expectedEx = ExpectedException.none();
+
+	@Test
+	public void shouldThrowIllegalArgumentExceptionFileNotEXist() {
+	    expectedEx.expect(IllegalArgumentException.class);
+	    expectedEx.expectMessage("Input file does not exist");
+	    testClass.readFile("../unittesting/src/test/resources/io/abcdoesnotexist.txt");
+	}
+	
+	@Test
+	public void shouldThrowIllegalArgumentExceptionEmptyFile() {
+	    expectedEx.expect(IllegalArgumentException.class);
+	    expectedEx.expectMessage("Given file is empty");
+	    testClass.readFile("../unittesting/src/test/resources/io/emptyfile.txt");
 	}
 }
