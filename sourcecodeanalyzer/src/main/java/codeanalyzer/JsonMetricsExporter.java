82c50f48ef5fcdd7dad0911e75d72207873b023e
Anna Mastori <t8190105@aueb.gr>
2022-06-15 22:15:25 +0300

    Create JsonMetricsExporter class

---
+++ sourcecodeanalyzer/src/main/java/codeanalyzer/JsonMetricsExporter.java
@@ -0,0 +1,24 @@
+package codeanalyzer;
+
+import java.util.Map;
+
+/**
+ * Implementation of MetricsExporter interface for json file.
+ * 
+ * @author mastori
+ *
+ */
+public class JsonMetricsExporter implements MetricsExporter {
+
+	/**
+	 * Writes metrics in a json file.
+	 * 
+	 * @param metrics  a map of metrics
+	 * @param filepath the path of the file
+	 */
+	@Override
+	public void writeFile(Map<String, Integer> metrics, String filepath) {
+		// Functionality not implemented yet
+	}
+
+}
