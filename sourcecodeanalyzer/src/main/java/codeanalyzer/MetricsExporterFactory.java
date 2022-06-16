1c2d434fb19f7b651598ffbd9833e6ef08c6bbb5
Anna Mastori <t8190105@aueb.gr>
2022-06-16 17:06:28 +0300

    Add case in MetricsExporter factory

--- sourcecodeanalyzer/src/main/java/codeanalyzer/MetricsExporterFactory.java
+++ sourcecodeanalyzer/src/main/java/codeanalyzer/MetricsExporterFactory.java
@@ -2,21 +2,25 @@ package codeanalyzer;
 
 /**
  * Factory class for the MetricsExporter interface.
+ * 
  * @author annamastori
  *
  */
 public class MetricsExporterFactory {
-	
+
 	/**
-	 * Creates a MetricsExporter that corresponds to the type of the output file. 
+	 * Creates a MetricsExporter that corresponds to the type of the output file.
+	 * 
 	 * @param fileType the type of the output file
 	 */
 	public MetricsExporter createMetricsExporter(String fileType) {
 		MetricsExporter ex;
 		if (fileType.equals("csv")) {
 			ex = new CSVMetricsExporter();
-		} else {
+		} else if (fileType.equals("json")) {
 			ex = new JsonMetricsExporter();
+		} else {
+			throw new IllegalArgumentException("Unknown export type : " + fileType);
 		}
 		return ex;
 	}
