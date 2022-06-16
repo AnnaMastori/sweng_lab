3c4dc45645da8d7b06c060d85a908fcf6d59d4b3
Anna Mastori <t8190105@aueb.gr>
2022-06-16 18:19:49 +0300

    Format CSVExporterTest

--- sourcecodeanalyzer/src/test/java/codeanalyzer/CSVExporterTest.java
+++ sourcecodeanalyzer/src/test/java/codeanalyzer/CSVExporterTest.java
@@ -9,7 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CSVExporterTest {
-	
+
 	MetricsExporter exp;
 
 	@Before
@@ -21,20 +21,20 @@ public class CSVExporterTest {
 	public void testWriteCSV() {
 		// create the metrics content
 		Map<String, Integer> metrics = new HashMap<>();
-		metrics.put("loc",30);
-		metrics.put("nom",5);
-		metrics.put("noc",2);
-		
+		metrics.put("loc", 30);
+		metrics.put("nom", 5);
+		metrics.put("noc", 2);
+
 		// generate and write the output file
 		String outputFilepath = "src/test/resources/output_metrics";
 		exp.writeFile(metrics, outputFilepath);
-		
+
 		// evaluate that the file exists
 		File outputFile = new File(outputFilepath + ".csv");
 		Assert.assertTrue(outputFile.exists());
-		
+
 		// delete the generated file
 		outputFile.delete();
 	}
-	
+
 }
